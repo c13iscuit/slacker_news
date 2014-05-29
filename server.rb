51ec66653e2e24c395107150e6fd086ce1865c0a
c13iscuit <barnett.chri@gmail.com>
2014-05-29 16:49:13 -0400

    Adding read/write to db function for comments.

--- server.rb
+++ server.rb
@@ -11,20 +11,37 @@ require 'net/http'
 def get_articles
   connection = PG.connect(dbname: 'slacker')
   results = connection.exec('SELECT * FROM articles')
-
   connection.close
+  results
+end
 
+def get_comments(parameter)
+  connection = PG.connect(dbname: 'slacker')
+  results = connection.exec_params('SELECT * FROM comments WHERE article_id = $1;', [parameter])
+  connection.close
   results
 end
 
 def save_article(content)
-  sql = "INSERT INTO articles (title, url, description,created_at) VALUES ($1,$2,$3,now())"
-
+  sql = "INSERT INTO articles (title, url, description,created_at) VALUES ($1,$2,$3,now());"
   connection = PG.connect(dbname: 'slacker')
   connection.exec_params(sql,content)
-
   connection.close
+end
 
+def save_comment(content) ******************
+  sql = "INSERT INTO comments (comment,created_at) VALUES ($1,now());"
+  connection = PG.connect(dbname: 'slacker')
+  connection.exec_params(sql,[content])
+  connection.close
+end
+
+def url_lookup(url)
+  sql = "SELECT url FROM articles WHERE url = $1;"
+  connection = PG.connect(dbname: 'slacker')
+  results = connection.exec_params(sql,[url])
+  connection.close
+  results
 end
 
 get "/" do
@@ -32,15 +49,14 @@ get "/" do
   erb :index
 end
 
-get "/comments" do
-  @comments = CSV.read("comments.csv")
-  erb :comments
+get "/:id/comments" do
+  @comments = get_comments(params[:id])
+  erb :article_comments
 end
 
 post "/comments" do
   @user_comment = params["comment"]
-  File.open("comments.csv", "a") do |file|
-    file.puts("#{@user_comment}")
+
   end
   redirect "/comments"
 end
@@ -50,16 +66,18 @@ get "/submission" do
 end
 
 post "/submission" do
+    @title = params["title"]
+    @url = params["url"]
+    @description = params["description"]
   #if Net::HTTP.get_response(URI.parse(url)).code == "200"
+  if url_lookup(params["url"]) == nil
+    save_article([@title,@url,@description])
+    redirect "/"
+  else
+    redirect "/url_error"
+  end
+end
 
-  @title = params["title"]
-  @url = params["url"]
-  @description = params["description"]
-  save_article([@title,@url,@description])
-  redirect "/"
-
-  # else
-  #   puts "Go fuck yourself!"
-  #   redirect '/submission'
-  # end
+get "/url_error" do
+  erb :url_error
 end
